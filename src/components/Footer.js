e61a4ca97e45a3c8854cf695f69b2109ba3e8a04
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2024-09-19 18:00:35 +0530

    footer updates

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <p>
+          &copy; {new Date().getFullYear()} Mohamed Sharfiras. All rights
+          reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
