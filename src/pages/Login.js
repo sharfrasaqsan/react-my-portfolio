97b6741c09437363251ae5241b11ab244dd3c647
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2025-07-30 07:32:56 +0530

    firebase auth, login, logout

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,84 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useAuth } from "../context/AuthContext";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const { setUser } = useAuth();
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      toast.error("Please enter email and password");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredentials.user;
+
+      const res = await getDoc(doc(db, "admin", user.uid));
+      if (!res.exists()) {
+        toast.error("User not found");
+        return;
+      }
+      setUser(res.data());
+      setEmail("");
+      setPassword("");
+      toast.success("Login successful");
+      navigate("/admin");
+    } catch (err) {
+      toast.error("Login failed", err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <section>
+      <div>
+        <h2>Admin Login</h2>
+        <form onSubmit={handleLogin}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            autoFocus
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            autoFocus
+          />
+          <button type="submit" disabled={loading}>
+            {loading ? "Logging in..." : "Login"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
